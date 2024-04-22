9996ca25fe57c702cf716d807fc1012531f36289
Walker <61227405+WalterClementsJr@users.noreply.github.com>
2024-04-22 23:13:16 +0700

    update app

--- analytics/config.py
+++ analytics/config.py
@@ -4,14 +4,14 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-db_username = os.environ["POSTGRES_USER"]
-POSTGRES_PASSWORD = os.environ["POSTGRES_PASSWORD"]
+db_username = os.environ.get("POSTGRES_USER", "admin")
+db_password = os.environ.get("POSTGRES_PASSWORD", "123456")
 db_host = os.environ.get("POSTGRES_HOST", "127.0.0.1")
 db_port = os.environ.get("POSTGRES_PORT", "5432")
 db_name = os.environ.get("POSTGRES_DB", "postgres")
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://{db_username}:{POSTGRES_PASSWORD}@{db_host}:{db_port}/{db_name}"
+app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}"
 
 db = SQLAlchemy(app)
 
